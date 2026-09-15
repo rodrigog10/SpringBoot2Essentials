2297a29b24fbc1c6c3a6b3b37d99a11f99d1a0a6
rodrigog10 <rodrigobisppo123@gmail.com>
2026-09-14 23:51:51 -0300

    estudando mais

--- src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
+++ src/main/java/academy/devdojo/springboot2/controller/AnimeController.java
@@ -1,12 +1,12 @@
 package academy.devdojo.springboot2.controller;
 
 import academy.devdojo.springboot2.domain.Anime;
-
-
 import academy.devdojo.springboot2.service.AnimeService;
 import academy.devdojo.springboot2.util.DateUtil;
+import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,17 +15,19 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 
+// Aqui é onde ocorre a primeira interação com a requisição do usuário com o sistema back-end.
+//Exemplo seguido: visualizar a lista de animes.
 @RestController
-@RequestMapping("animes")
+@RequestMapping("anime")
 @Log4j2
 @RequiredArgsConstructor
 
 public class AnimeController {
+
     private final DateUtil dateUtil;
     private final AnimeService animeService;
 
-
-    @GetMapping
+    @GetMapping(path = "list")
     public List<Anime> list(){
         log.info(dateUtil.FormatLocalDateTimeToDataBaseStyle(LocalDateTime.now()));
         return animeService.listall();
--- src/main/java/academy/devdojo/springboot2/repository/AnimeRepository.java
+++ src/main/java/academy/devdojo/springboot2/repository/AnimeRepository.java
@@ -5,6 +5,7 @@ import academy.devdojo.springboot2.domain.Anime;
 
 import java.util.List;
 
+
 public interface AnimeRepository {
     List<Anime> listAll();
 }
